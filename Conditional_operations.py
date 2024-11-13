9d7221506497b8ba50a02becf725412fc1a8831c
Gopi <tambigopijs@gmail.com>
2024-11-13 22:23:37 +0530

    comming code and example

--- Conditional_operations.py
+++ Conditional_operations.py
@@ -7,7 +7,19 @@ emp = pd.read_table('D:/desktop/Data.txt', sep=' ')
 print(emp.dtypes)
 
 #Apply the condition for Bonus
-emp.loc[(emp['sale']>500,'Bonus')]=emp['sale']*0.7
-emp.loc[(emp['sale']==500,'Bonus')]=emp['sale']*0.5
-emp.loc[(emp['sale']<500,'Bonus')]=emp['sale']*0.3
+emp.loc[(emp['sale']>500,'addings')]=emp['sale']*0.7
+emp.loc[(emp['sale']==500,'addings')]=emp['sale']*0.5
+emp.loc[(emp['sale']<500,'addings')]=emp['sale']*0.3
+print(emp)
+
+emp.loc[(emp['sale']>=500,'Bonus')]=emp['sale']*0.5
+emp.loc[((emp['sale']>350)&(emp['sale']<500),'Bonus')]=emp['sale']*0.4
+emp.loc[((emp['sale']<=350)&(emp['sale']>200),'Bonus')]=emp['sale']*0.3
+emp.loc[(emp['sale']<200,'Bonus')]=emp['sale']*0.2
+print(emp)
+
+emp.loc[(emp['sale']>=500,'Rating')]='Platinum'
+emp.loc[((emp['sale']>350)&(emp['sale']<500),'Rating')]='Glod'
+emp.loc[((emp['sale']<=350)&(emp['sale']>200),'Rating')]='Silver'
+emp.loc[(emp['sale']<200,'Rating')]='Non'
 print(emp)
