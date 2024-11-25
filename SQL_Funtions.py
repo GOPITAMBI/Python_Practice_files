24370b5c0d8991174a9e886a0f886698fe2c398e
Gopi <tambigopijs@gmail.com>
2024-11-25 13:48:57 +0530

    New Code

--- SQL_Funtions.py
+++ SQL_Funtions.py
@@ -9,3 +9,23 @@ sqldf('select * from Dm')
 Dm2= sqldf('select Name, Age, Sex from Dm')
 print(Dm2)
 
+# Sorting orders
+Dm3=sqldf('select * from Dm order by Age')
+print(Dm3)
+Dm4=sqldf('select * from Dm order by Age desc')
+print(Dm4)
+Dm5=sqldf('select * from Dm order by Sex,Age desc')
+print(Dm5)
+
+# Report part of the conditional based(> < >= <= = !=)
+Dm6=sqldf('select * from Dm where Age>=14')
+print(Dm6)
+Dm7=sqldf('select * from Dm where Age>=15 and Sex="M" ')
+print(Dm7)
+Dm8=sqldf('select * from Dm where Age=11 or Age=14')
+print(Dm8)
+Dm9=sqldf('select * from Dm where Age in (11,13,16)')
+print(Dm9)
+Dm10=sqldf('select * from Dm where Age between 15 and 17')
+print(Dm10)
+
